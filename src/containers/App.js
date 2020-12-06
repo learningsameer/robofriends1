313b1b5b91a11bcd44f1f490eb3bcfdd75a6665c
learningsameer <learningsameer@gmail.com>
2020-12-06 09:19:49 -0500

    change https

--- src/containers/App.js
+++ src/containers/App.js
@@ -31,7 +31,7 @@ class App extends Component{
 
 	componentDidMount(){
 		// console.log("componentDidMount");
-		fetch('http://jsonplaceholder.typicode.com/users')
+		fetch('https://jsonplaceholder.typicode.com/users')
 			.then(response=> response.json())
 			.then(users=>this.setState({robots:users}));
 		
